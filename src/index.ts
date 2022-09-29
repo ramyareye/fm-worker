6d827bf095d2292da90cfde6f91cf0b704fc3132
Reza Babaei <ramyariran@gmail.com>
2022-09-29 14:23:06 +0700

    feat: read list from b4a

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,45 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  PARSE_SERVER: string;
+  PARSE_KEY1: string;
+  PARSE_KEY2: string;
+}
+
+async function gatherResponse(response: Response) {
+  const { headers } = response;
+  const contentType = headers.get("content-type") || "";
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await response.json());
+  }
+  return response.text();
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const response = await fetch(env.PARSE_SERVER + "/functions/list", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Parse-Application-Id": env.PARSE_KEY1,
+        "X-Parse-REST-API-Key": env.PARSE_KEY2,
+      },
+    });
+
+    const results = await gatherResponse(response);
+
+    return new Response(results);
+  },
 };
